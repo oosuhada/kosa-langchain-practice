7eaf140ddeaaa952c48ca74143f017d48ce9d09c
oosuhada <185910926+oosuhada@users.noreply.github.com>
2026-06-03 19:41:29 +0900

    feat(core): implement main logic

---
+++ python/chap08/sec02_import/exam01/main.py
@@ -0,0 +1,18 @@
+# import sub1, sub2  - 함께 import 가능하지만 추천하지 않음
+
+import sub1
+import sub2
+
+if __name__ == "__main__":
+    # 모듈의 함수 호출
+    sub1.fun_a()
+    sub2.fun_b()
+    # 이 코드만 봐서는 객체인지 모듈인지 알 수 없음
+    # import를 보고 sub1과 sub2가 모듈이라는 것을 알아낸 후 .을 통해 모듈안에 정의된 함수 호출이라는 것 파악해야함
+
+    # 모듈의 클래스를 이용해서 객체 생성
+    obj_a = sub1.A()
+    obj_b = sub2.A()
+
+    # sub1.obj_a.method()  # 객체 안에 정의된 메소드 호출
+    # sub2.obj_b.method()  # 객체 안에 정의된 메소드 호출
